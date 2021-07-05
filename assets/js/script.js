81d01caef0052403739d743c3dec5e6dee2dfcfb
Josh Gumperz <joshg3023@gmail.com>
2021-07-04 17:55:01 -0700

    added temperature, humidity, and wind speed to display in content-container, added classes for uv index

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,19 @@
 var apiKey = "4fd1df31154eabf381d8360e8c92b155"
 var cityName = $("#city-name")
 var cityInput = $(".city-input")
+var contentEL = $(".content")
 var displayCityName = $("#display-city-name")
 var displayCurrentWeather = $(".content-container")
 var displayIcon = $(".custom-icon")
+var displayTemp = $(".temp")
+var displayHumidity = $(".humidity")
+var windSpeed = $("#wind-speed")
+var uvIndex = $("#uv-index")
 var currentDate = moment().format("l")          
 
 cityInput.on("submit", function(event) {
     event.preventDefault()
+    contentEL.css("display", "initial")
     var city = cityName.val()
     var URL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey
 
@@ -19,10 +25,13 @@ cityInput.on("submit", function(event) {
         console.log(data)
         var { name } = data
         var { icon } = data.weather[0]
-        var { temp } = data.main
+        var { temp, humidity } = data.main
+        var { speed } = data.wind
         displayCityName.text(name + " " + currentDate)
         displayIcon.css("display", "initial")
         displayIcon.attr("src", "http://openweathermap.org/img/wn/" + icon + "@2x.png")
-
+        displayTemp.text("Temperature: " + temp)
+        displayHumidity.text("Humidity: " + humidity + "%")
+        windSpeed.text("Wind Speed: " + speed + " MPH")
     })
 })
