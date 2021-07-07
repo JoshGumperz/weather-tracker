caca8efb02e83724f676b6dfad09dfbe79d2a7d7
Josh Gumperz <joshg3023@gmail.com>
2021-07-06 18:09:23 -0700

    created code to store the last 5 cities that have been searched to local storage, and display the weather information for those cities when the user clicks the button for that city

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var apiKey = "4fd1df31154eabf381d8360e8c92b155"
 var cityName = $("#city-name")
 var cityInput = $(".city-input")
+var searchedCities = $(".searched-cities")
 var contentEL = $(".content")
 var displayCityName = $(".display-city-name")
 var currentWeather = $(".content-container")
@@ -40,19 +41,52 @@ var fiveDayForecast = {
 }
 forecastDates = [$("#city-name-day-one"), $("#city-name-day-two"), $("#city-name-day-three"), $("#city-name-day-four"), $("#city-name-day-five"),]
 var currentDate = moment().format("l")
+var cityNames = JSON.parse(localStorage.getItem("Cities")) || []
 
 cityInput.on("submit", function (event) {
     event.preventDefault()
-    contentEL.css("display", "initial")
     var city = cityName.val()
     var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey
+    saveCities(city)
+    getWeatherData(weatherURL)
+})
 
-    fetch(weatherURL)
+function saveCities(city) {
+    if (!cityNames.includes(city)) {
+    cityNames.unshift(city)
+    }
+    if (cityNames.length === 5){
+        cityNames.splice(4, 1)
+    }
+    console.log(cityNames)
+    localStorage.setItem("Cities", JSON.stringify(cityNames))
+    return city
+    
+}
+
+function displaySavedCities() {
+    for (var i = 0; i < cityNames.length; i++) {
+        searchedCities.append($("<li>").text(cityNames[i]).addClass("btn"))
+    }
+}
+
+
+searchedCities.on("click", function(event){
+    var element = event.target;
+    if(element.matches("li")){
+        var city = element.textContent
+        var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey
+        getWeatherData(weatherURL)
+    }
+})
+
+function getWeatherData(URL){
+    contentEL.css("display", "initial")
+    fetch(URL)
         .then(function (response) {
             return response.json()
         })
         .then(function (data) {
-            console.log(data)
             currentIcon.css("display", "initial")
             var { name } = data
             var { icon } = data.weather[0]
@@ -72,7 +106,6 @@ cityInput.on("submit", function (event) {
                     return response.json()
                 })
                 .then(function (data) {
-                    console.log(data)
                     var displayIcons = $(".custom-icon")
                     displayIcons.css("display", "initial")
                     for (var i = 1; i < 6; i++) {
@@ -121,4 +154,5 @@ cityInput.on("submit", function (event) {
                     }
                 })
         })
-})
+}
+displaySavedCities()
